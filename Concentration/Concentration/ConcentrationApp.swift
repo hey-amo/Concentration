a92842383e5f83a833744343efa981017d41dbdc
AD <silentflute.ad@gmail.com>
2025-11-01 05:53:29 +0000

    Initial project build

---
+++ Concentration/Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Amarjit on 01/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
